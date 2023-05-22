c1efc31c53eadf51ba7fca06d652ec30e12a39d7
Lilit Galstyan <lgalstyan@griddynamics.com>
2023-05-22 19:05:09 +0400

    add rest version

--- src/main/java/grid/onlineshop/sweetland/dto/response/JwtResponse.java
+++ src/main/java/grid/onlineshop/sweetland/dto/response/JwtResponse.java
@@ -9,7 +9,7 @@ public class JwtResponse implements Serializable {
 
     private static final long serialVersionUID = -8091879091924046844L;
 
-    private final String jwttoken;
+    private final String jwtToken;
 
 }
 
